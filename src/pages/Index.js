f8f6b850e2723a01af275e48b06112fb1490ff33
sashaminorr <sashaminorr@protonmail.com>
2022-05-30 22:56:20 +0300

    first commit

---
+++ src/pages/Index.js
@@ -0,0 +1,13 @@
+
+import Navbar from '../components/Navbar.js'
+import FileSelector from '../components/FileSelector.js'
+
+export function Index(){
+    return (
+        <>
+            <Navbar title="filetransfer"/>
+            <FileSelector/>
+        </>
+
+    )
+}
